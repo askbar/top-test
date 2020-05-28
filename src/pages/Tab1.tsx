7cff0d1d9d98f0cd285d5228073cd30e91ece83f
ask josephsen <syj@topdanmark.dk>
2020-05-28 10:52:28 +0200

    adding internal npm module

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import React from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+// import ExploreContainer from "../components/ExploreContainer";
+import "../types.d.ts";
+import CalendarTimeSlot from "calendar-time-slot";
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
+  const slots = [
+    {
+      id: 123,
+      startTime: "2019-11-01T08:00:13.164Z",
+      endTime: "2019-11-01T08:25:13.164Z",
+    },
+    {
+      id: 234,
+      startTime: "2019-11-01T09:00:13.164Z",
+      endTime: "2019-11-01T09:20:13.164Z",
+    },
+
+    {
+      id: 345,
+      startTime: "2019-11-12T12:00:13.164Z",
+      endTime: "2019-11-12T12:50:13.164Z",
+    },
+    {
+      id: 567,
+      startTime: "2019-11-12T13:00:13.164Z",
+      endTime: "2019-11-12T13:20:13.164Z",
+    },
+    {
+      id: 678,
+      startTime: "2019-11-12T14:00:13.164Z",
+      endTime: "2019-11-12T14:40:13.164Z",
+    },
+  ];
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,7 +54,7 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <CalendarTimeSlot slots={slots} />
       </IonContent>
     </IonPage>
   );
