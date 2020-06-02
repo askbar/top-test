f5c2db4935f1975094f6f75645838a11562de6c9
ask josephsen <askbar@gmail.com>
2020-06-02 11:07:01 +0200

    adding native barcode scanner

--- readme.md
+++ readme.md
@@ -4,6 +4,8 @@ cd myApp
 npm run build
 
 ionic capacitor add ios
-npx cap open ios    // open x-code and run 
+npx cap open ios    // open x-code 
+ionic capacitor run ios -l --external // open x-code and run - live update
+pod install // in ios/App folder if build 
 
-ionic serve
+ionic serve  // run web version
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -4,18 +4,25 @@ import {
   IonHeader,
   IonPage,
   IonTitle,
-  IonToolbar,
+  IonToolbar, IonFab, IonFabButton, IonFabList, IonIcon 
 } from "@ionic/react";
+import { logoFacebook, logoTwitter, logoYoutube, logoPwa, logoNpm, logoIonic, logoGithub, logoJavascript, barcode, logoVimeo, logoChrome, logoReact } from 'ionicons/icons';
 import "../types.d.ts";
 import "./Tab1.css";
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 const Tab1: React.FC = () => {
+
+  const openScanner = async () => {
+    const data = await BarcodeScanner.scan();
+    alert(`Barcode data: ${data.text}`);
+  };
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Test</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
@@ -24,6 +31,58 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
+
+        <IonFab vertical="center" horizontal="center">
+      <IonFabButton>Share</IonFabButton>
+      <IonFabList side="top">
+        <IonFabButton onClick={openScanner}>
+          <IonIcon icon={barcode} />
+        </IonFabButton>
+        <IonFabButton>
+          <IonIcon icon={logoTwitter} />
+        </IonFabButton>
+        <IonFabButton>
+          <IonIcon icon={logoYoutube} />
+        </IonFabButton>
+      </IonFabList>
+
+      <IonFabList side="end">
+        <IonFabButton>
+          <IonIcon icon={logoPwa} />
+        </IonFabButton>
+        <IonFabButton>
+          <IonIcon icon={logoNpm} />
+        </IonFabButton>
+        <IonFabButton>
+          <IonIcon icon={logoIonic} />
+        </IonFabButton>
+      </IonFabList>
+
+      <IonFabList side="bottom">
+        <IonFabButton>
+          <IonIcon icon={logoGithub} />
+        </IonFabButton>
+        <IonFabButton>
+          <IonIcon icon={logoJavascript} />
+        </IonFabButton>
+        <IonFabButton>
+          <IonIcon icon={logoFacebook} />
+        </IonFabButton>
+      </IonFabList>
+
+      <IonFabList side="start">
+        <IonFabButton>
+          <IonIcon icon={logoVimeo} />
+        </IonFabButton>
+        <IonFabButton>
+          <IonIcon icon={logoChrome} />
+        </IonFabButton>
+        <IonFabButton>
+          <IonIcon icon={logoReact} />
+        </IonFabButton>
+      </IonFabList>
+    </IonFab>
+
       </IonContent>
     </IonPage>
   );
